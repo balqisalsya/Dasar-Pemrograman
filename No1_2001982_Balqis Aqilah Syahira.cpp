789530a40324a82cecf1d50bc1b0f6c0e872819c
Balqis Aqilah Syahira <77434299+balqisalsya@users.noreply.github.com>
2021-06-14 09:48:17 +0700

    program untuk menukar nilai inputan integer

---
+++ No1_2001982_Balqis Aqilah Syahira.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main ()
+{
+    float a,b;
+    cout << "Input bilangan A = ";
+    cin >>a;
+    cout << "Input bilangan B = ";
+    cin >>b;
+    cout << " "<<endl;
+    cout << " "<<endl;
+    cout << "Setelah ditukar menjadi:" <<endl;
+    cout << "Bilangan A = "<<b<<endl;
+    cout << "Bilangan B = "<<a<<endl;
+
+    return 0;
+}
